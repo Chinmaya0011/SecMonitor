26043ae22d86a3c00677c7062f572f1d18aab360
Chinmaya Kumar Mishra <103204520+Chinmaya0011@users.noreply.github.com>
2026-04-26 18:40:08 +0530

    Create Hiis.php

---
+++ Hiis.php
@@ -0,0 +1,21 @@
+<?php
+// Simple output
+echo "Hello, Chinu! <br>";
+
+// Variables
+$name = "Chinu";
+$age = 22;
+
+// Condition
+if ($age >= 18) {
+    echo "$name is an adult.<br>";
+} else {
+    echo "$name is not an adult.<br>";
+}
+
+// Loop
+echo "Numbers from 1 to 5:<br>";
+for ($i = 1; $i <= 5; $i++) {
+    echo $i . "<br>";
+}
+?>
